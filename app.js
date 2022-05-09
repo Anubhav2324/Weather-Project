efa37ab0b036bf7b3b39f4a027f367686063de72
Anubhav2324 <kumaranubhav8844@gmail.com>
2022-05-09 23:21:14 +0530

    Commit-2

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const https = require('https');
+const bodyParser = require('body-parser');
+
 const app = express();
-const https = require('https')
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/',(req,res)=>{
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=Deoghar&appid=16de24b4888f3340b945750ae1eeffdd&units=metric'
+    res.sendFile(__dirname +'/index.html' )
+
+})
+
+app.post('/',(req,res)=>{
+    const query = req.body.cityName
+    const apiKey = '16de24b4888f3340b945750ae1eeffdd'
+    const unit = 'metric'
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q='+query+'&appid='+apiKey+'&units='+unit
     https.get(url,(response)=>{
         console.log(response.statusCode);
 
@@ -15,7 +26,7 @@ app.get('/',(req,res)=>{
            console.log(description);
            const icon = weatherData.weather[0].icon
            const imgURL = 'http://openweathermap.org/img/wn/'+icon+'@2x.png'
-           res.write('<h1>Temp in Deoghar is '+ temp + ' degress Celcius.</h1>')
+           res.write('<h1>Temp in '+query+' is '+ temp + ' degress Celcius.</h1>')
            res.write('<p>The weather is currently '+ description +'</p>')
            res.write('<img src='+imgURL+'>')
            res.send()
@@ -23,6 +34,8 @@ app.get('/',(req,res)=>{
     })
 
 })
+
+
 
 
 
